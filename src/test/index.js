f81e6114ccc0e2de5fd4e571c478ed5b2cd88df8
fangxu <864109504@qq.com>
2016-12-09 17:07:37 +0800

    1212

---
+++ src/test/index.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Administrator on 2016/12/8.
+ */
+
+require("./index.scss");
+var tpl = require ("./index.xtpl");
+alert("123");
---
+++ src/test/webpack.config.js
@@ -0,0 +1,46 @@
+
+var webpack = require('webpack');
+// var commonsPlugin = new webpack.optimize.CommonsChunkPlugin('common.js');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+    //插件项
+    // plugins: [commonsPlugin, new ExtractTextPlugin("[name].css")],
+    plugins: [
+        new ExtractTextPlugin("[name].css"),
+        // new webpack.optimize.UglifyJsPlugin({
+        //     compress: {
+        //         warnings : false
+        //     },
+        //     sourceMap : false
+        // })
+    ],
+    //页面入口文件配置
+    entry: {
+        index : './index.js'
+    },
+    //入口文件输出配置
+    output: {
+        path: '../../build',
+        filename: '[name].js'
+    },
+    module: {
+        //加载器配置
+        loaders: [
+            { test: /\.scss$/, loader: ExtractTextPlugin.extract('style', 'css!sass')},
+            { test: /\.js$/, loader: 'jsx-loader?harmony' },
+            { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'},
+            { test : /\.html|tpl|xtpl$/, loader : "html?-minimize"}
+        ]
+    },
+    // //其它解决方案配置
+    // resolve: {
+    //     root: 'E:/github/flux-example/src', //绝对路径
+    //     extensions: ['', '.js', '.json', '.scss'],
+    //     alias: {
+    //         AppStore : 'js/stores/AppStores.js',
+    //         ActionType : 'js/actions/ActionType.js',
+    //         AppAction : 'js/actions/AppAction.js'
+    //     }
+    // }
+};
