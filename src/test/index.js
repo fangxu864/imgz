d7fd4eb5e38d9cb38a6aa8dba2dfc4fc0d7d2671
fangxu <864109504@qq.com>
2016-12-12 09:30:57 +0800

    no message

--- src/test/index.js
+++ src/test/index.js
@@ -2,6 +2,24 @@
  * Created by Administrator on 2016/12/8.
  */
 
+var Cropper = require ("cropperjs");
+
 require("./index.scss");
-var tpl = require ("./index.xtpl");
-alert("123");
+
+window.onload = function () {
+    var image = document.getElementById('image1');
+    var cropper = new Cropper(image, {
+        aspectRatio: 16 / 9,
+        crop: function(e) {
+            console.log(e.detail.x);
+            console.log(e.detail.y);
+            console.log(e.detail.width);
+            console.log(e.detail.height);
+            console.log(e.detail.rotate);
+            console.log(e.detail.scaleX);
+            console.log(e.detail.scaleY);
+        }
+    });
+}
+
+
